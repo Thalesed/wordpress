55922b3f9cd7d767cc7ecad4731dc277b0441bb6
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2023-06-26 15:30:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+get_header();
+?>
+<main id="main-content" class="site-main">
+
+        <div class = "contato">
+            <div class = "imagem-contato">
+                <img src="EBA-Logo2.jpg" class="imagem-logo-eba">
+            </div>
+            <div class = "texto-contato">
+                <h2> Setor de Tecnologia da Informação </h2>
+                <p> Email: ti@eba.ufmg.br </p>
+                <p> Telefone: 3409-5266 </p>
+            </div>
+        </div>
+	<button>
+		Aperte
+	</button>
+	<a href="https://eba.ufmg.br">
+		Link
+	</a>
+</main>
